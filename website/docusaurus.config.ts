2f778b0121bd8aa0e1526ca501592bf98692fadc
worjs <whworjs777@gmail.com>
2025-09-29 14:29:24 +0900

    domain issue

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   url: "https://signals-protocol.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/signals-v0/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   organizationName: "signals-protocol", // Usually your GitHub org/user name.
